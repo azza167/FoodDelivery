dafa03aa9af58337af361f535817c709a7b923af
nadah79 <nada.hesham.sh@gmail.com>
2022-10-29 15:24:47 +0200

    add products

---
+++ src/assets/products.js
@@ -0,0 +1,157 @@
+const products = [
+    {
+      id: "01",
+      title: "Cheese pizza",
+      price: 55.0,
+      image01: "../assets/images/pizza01.jpg",
+      category: "pizza",
+  
+      desc: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Soluta ad et est, fugiat repudiandae neque illo delectus commodi magnam explicabo autem voluptates eaque velit vero facere mollitia. Placeat rem, molestiae error obcaecati enim doloribus impedit aliquam, maiores qui minus neque. ",
+    },
+  
+    {
+      id: "02",
+      title: "Vegetarian Pizza",
+      price: 115.0,
+      image01: "../assets/images/pizza02.jpg",
+      category: "Pizza",
+  
+      desc: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Soluta ad et est, fugiat repudiandae neque illo delectus commodi magnam explicabo autem voluptates eaque velit vero facere mollitia. Placeat rem, molestiae error obcaecati enim doloribus impedit aliquam, maiores qui minus neque.",
+    },
+    {
+        id: "03",
+        title: "Seafood Pizza",
+        price: 115.0,
+        image01: "../assets/images/pizza03.jpg",
+        category: "Pizza",
+    
+        desc: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Soluta ad et est, fugiat repudiandae neque illo delectus commodi magnam explicabo autem voluptates eaque velit vero facere mollitia. Placeat rem, molestiae error obcaecati enim doloribus impedit aliquam, maiores qui minus neque.",
+
+    },
+    {
+        id: "04",
+        title: "Pizza With Mushroom",
+        price: 115.0,
+        image01: "../assets/images/pizza04.jpg",
+        category: "Pizza",
+        desc: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Soluta ad et est, fugiat repudiandae neque illo delectus commodi magnam explicabo autem voluptates eaque velit vero facere mollitia. Placeat rem, molestiae error obcaecati enim doloribus impedit aliquam, maiores qui minus neque.",
+
+    },
+    {
+        id: "05",
+        title: "Maxican Green Wave",
+        price: 115.0,
+        image01: "../assets/images/pizza05.jpg",
+        category: "Pizza",
+        desc: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Soluta ad et est, fugiat repudiandae neque illo delectus commodi magnam explicabo autem voluptates eaque velit vero facere mollitia. Placeat rem, molestiae error obcaecati enim doloribus impedit aliquam, maiores qui minus neque.",
+
+    },
+    {
+        id: "06",
+        title: "Double Cheese Margherita",
+        price: 115.0,
+        image01: "../assets/images/pizza06.jpg",
+        category: "Pizza",
+        desc: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Soluta ad et est, fugiat repudiandae neque illo delectus commodi magnam explicabo autem voluptates eaque velit vero facere mollitia. Placeat rem, molestiae error obcaecati enim doloribus impedit aliquam, maiores qui minus neque.",
+
+    },
+    {
+        id: "07",
+        title: "Pizza With Mushroom",
+        price: 115.0,
+        image01: "../assets/images/pizza07.jpg",
+        category: "Pizza",
+        desc: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Soluta ad et est, fugiat repudiandae neque illo delectus commodi magnam explicabo autem voluptates eaque velit vero facere mollitia. Placeat rem, molestiae error obcaecati enim doloribus impedit aliquam, maiores qui minus neque.",
+
+    },
+    {
+        id: "08",
+        title: "Pizza With Mushroom",
+        price: 115.0,
+        image01: "../assets/images/pizza08.jpg",
+        category: "Pizza",
+        desc: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Soluta ad et est, fugiat repudiandae neque illo delectus commodi magnam explicabo autem voluptates eaque velit vero facere mollitia. Placeat rem, molestiae error obcaecati enim doloribus impedit aliquam, maiores qui minus neque.",
+
+    },
+    {
+        id: "09",
+        title: "Pizza With Mushroom",
+        price: 115.0,
+        image01: "../assets/images/pizza09.jpg",
+        category: "Pizza",
+        desc: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Soluta ad et est, fugiat repudiandae neque illo delectus commodi magnam explicabo autem voluptates eaque velit vero facere mollitia. Placeat rem, molestiae error obcaecati enim doloribus impedit aliquam, maiores qui minus neque.",
+
+    },
+    {
+        id: "10",
+        title: "Pizza With Mushroom",
+        price: 115.0,
+        image01: "../assets/images/pizza10.jpg",
+        category: "Pizza",
+        desc: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Soluta ad et est, fugiat repudiandae neque illo delectus commodi magnam explicabo autem voluptates eaque velit vero facere mollitia. Placeat rem, molestiae error obcaecati enim doloribus impedit aliquam, maiores qui minus neque.",
+
+    },
+    {
+        id: "11",
+        title: "Pizza With Mushroom",
+        price: 115.0,
+        image01: "../assets/images/pizza11.jpg",
+        category: "Pizza",
+        desc: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Soluta ad et est, fugiat repudiandae neque illo delectus commodi magnam explicabo autem voluptates eaque velit vero facere mollitia. Placeat rem, molestiae error obcaecati enim doloribus impedit aliquam, maiores qui minus neque.",
+
+    },
+    {
+        id: "12",
+        title: "Pizza With Mushroom",
+        price: 115.0,
+        image01: "../assets/images/pizza12.jpg",
+        category: "Pizza",
+        desc: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Soluta ad et est, fugiat repudiandae neque illo delectus commodi magnam explicabo autem voluptates eaque velit vero facere mollitia. Placeat rem, molestiae error obcaecati enim doloribus impedit aliquam, maiores qui minus neque.",
+
+    },
+    {
+        id: "13",
+        title: "Pizza With Mushroom",
+        price: 115.0,
+        image01: "../assets/images/pizza13.jpg",
+        category: "Pizza",
+        desc: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Soluta ad et est, fugiat repudiandae neque illo delectus commodi magnam explicabo autem voluptates eaque velit vero facere mollitia. Placeat rem, molestiae error obcaecati enim doloribus impedit aliquam, maiores qui minus neque.",
+
+    },
+    {
+        id: "14",
+        title: "Pizza With Mushroom",
+        price: 115.0,
+        image01: "../assets/images/pizza14.jpg",
+        category: "Pizza",
+        desc: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Soluta ad et est, fugiat repudiandae neque illo delectus commodi magnam explicabo autem voluptates eaque velit vero facere mollitia. Placeat rem, molestiae error obcaecati enim doloribus impedit aliquam, maiores qui minus neque.",
+
+    },
+    {
+        id: "15",
+        title: "Pizza With Mushroom",
+        price: 115.0,
+        image01: "../assets/images/pizza15.jpg",
+        category: "Pizza",
+        desc: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Soluta ad et est, fugiat repudiandae neque illo delectus commodi magnam explicabo autem voluptates eaque velit vero facere mollitia. Placeat rem, molestiae error obcaecati enim doloribus impedit aliquam, maiores qui minus neque.",
+
+    },
+    {
+        id: "16",
+        title: "Pizza With Mushroom",
+        price: 115.0,
+        image01: "../assets/images/pizza16.jpg",
+        category: "Pizza",
+        desc: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Soluta ad et est, fugiat repudiandae neque illo delectus commodi magnam explicabo autem voluptates eaque velit vero facere mollitia. Placeat rem, molestiae error obcaecati enim doloribus impedit aliquam, maiores qui minus neque.",
+
+    },
+    {
+        id: "17",
+        title: "Pizza With Mushroom",
+        price: 115.0,
+        image01: "../assets/images/pizza17.jpg",
+        category: "Pizza",
+        desc: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Soluta ad et est, fugiat repudiandae neque illo delectus commodi magnam explicabo autem voluptates eaque velit vero facere mollitia. Placeat rem, molestiae error obcaecati enim doloribus impedit aliquam, maiores qui minus neque.",
+
+    },
+];
