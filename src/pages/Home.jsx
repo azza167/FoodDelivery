8049a8a2df2ba44b350f1337cc6cf9d96ee77562
M-Metaw3 <mizoomizoo161@gmail.com>
2022-10-31 17:42:33 +0200

    aaa

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -4,7 +4,7 @@ import React, { useState } from 'react'
 import ProductCard from "../component/ProuductCard/ProductCard.jsx";
 import Category from "../component/Category/Category";
 import { Col, Container, Row } from "reactstrap";
-
+import hero from '../assets/images/hero.png'
 const Home = () => {
 
 const [allproducts,setAllProducts]=useState(products);
@@ -14,6 +14,23 @@ const [allproducts,setAllProducts]=useState(products);
     
     <Container> 
       <Row>
+        <Col lg='6' md='6' className="hero-details">
+        <h4>Easy way to make an order </h4>
+        <h1 color="">Hungry?<span>Just order foood </span><span>at your door</span></h1>
+        </Col >
+        <Col lg='6' md='6' className="hero-image ">
+        <img src={hero}  className="w-100" alt="" />
+        </Col>
+
+
+
+
+
+
+
+
+
+
       <Category/>
 
         {allproducts.map((item)=>(
