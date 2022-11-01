bcf4225e405187a644df1ee4c5b1929dceb028a6
azza167 <azza.ezzat167@gmail.com>
2022-11-01 18:38:41 +0200

    e

--- src/component/Layout/Layout.jsx
+++ src/component/Layout/Layout.jsx
@@ -4,6 +4,7 @@ import Header from "../Header/header";
 import Footer from "../Footer/footer";
 import Routers from "../../routes/Routers";
 import Checkout from "../../pages/Checkout";
+import { Link } from "react-router-dom";
 // import { Route } from "react-router-dom";
 
 
@@ -14,7 +15,9 @@ const Layout = () => {
 
     <Header/>
     <Routers/>
-    <Checkout/>
+    <button >
+                    <Link to="/checkout">checkout</Link>
+                  </button>
      <Footer/>
 
     </div>
