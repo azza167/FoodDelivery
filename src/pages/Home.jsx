2a716a98388aee15e141d23437f137e1d24211de
nadah79 <nada.hesham.sh@gmail.com>
2022-11-01 18:44:06 +0200

    updates

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,38 +1,29 @@
-<<<<<<< HEAD
-// import products from "../assets/products.js";
+import products from "../assets/products.js";
 // import { useSelector ,useDispatch  } from 'react-redux'
 // import { useEffect } from "react";
-import React  from 'react'
 //  import ProductCard from "../component/ProuductCard/ProductCard.jsx";
-=======
-import products from "../assets/products.js";
 import React, { useState } from 'react'
 import ProductCard from "../component/ProuductCard/ProductCard.jsx";
->>>>>>> d39a5cf480d6b8850d8cd6a1d2ac61130cca7b14
 import Category from "../component/Category/Category";
 import { Col, Container, Row } from "reactstrap";
 import hero from '../assets/images/hero.png'
+import { Link } from "react-router-dom";
 const Home = () => {
 
-<<<<<<< HEAD
+
 // const [allproducts,setAllProducts]=useState(products);
 
-  return (
     
-    <Container>      
-=======
 const [allproducts]=useState(products);
 
 
   return (
     
     <Container> 
->>>>>>> d39a5cf480d6b8850d8cd6a1d2ac61130cca7b14
       <Row>
         <Col lg='6' md='6' className="hero-details">
         <h4>Easy way to make an order </h4>
         <h1 color="">Hungry?<span>Just order foood </span><span>at your door</span></h1>
-<<<<<<< HEAD
         </Col>
         <p>
                   Lorem ipsum dolor sit amet, consectetur adipisicing elit. Qui
@@ -48,14 +39,10 @@ const [allproducts]=useState(products);
                   </button>
                   
                 </div>
-=======
-        </Col >
->>>>>>> d39a5cf480d6b8850d8cd6a1d2ac61130cca7b14
         <Col lg='6' md='6' className="hero-image ">
         <img src={hero}  className="w-100" alt="" />
         </Col>
 
-<<<<<<< HEAD
 
 
       <Category/>
@@ -66,7 +53,6 @@ const [allproducts]=useState(products);
       <Category/>
 
         {allproducts.map((item)=>(
->>>>>>> d39a5cf480d6b8850d8cd6a1d2ac61130cca7b14
           <Col lg="3" md="4" sm="6" xs="6" key={item.id}>
           <ProductCard item={item}/>
 
