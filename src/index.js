56117a7dbfee2f48b46b8de66532f5925ec481f9
nadah79 <nada.hesham.sh@gmail.com>
2022-11-01 04:39:15 +0200

    updates

--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@ import ReactDOM from 'react-dom/client';
 import App from './App';
 import {BrowserRouter as Router} from 'react-router-dom'
 import "bootstrap/dist/css/bootstrap.min.css";
-import "remixicon/fonts/remixicon.css";
+ import "remixicon/fonts/remixicon.css";
 
 
 
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,13 +1,13 @@
 import products from "../assets/products.js";
 
 import React, { useState } from 'react'
-import ProductCard from "../component/ProuductCard/ProductCard.jsx";
+// import ProductCard from "../component/ProuductCard/ProductCard.jsx";
 import Category from "../component/Category/Category";
 import { Col, Container, Row } from "reactstrap";
 import hero from '../assets/images/hero.png'
 const Home = () => {
 
-const [allproducts,setAllProducts]=useState(products);
+// const [allproducts,setAllProducts]=useState(products);
 
 
   return (
@@ -17,23 +17,14 @@ const [allproducts,setAllProducts]=useState(products);
         <Col lg='6' md='6' className="hero-details">
         <h4>Easy way to make an order </h4>
         <h1 color="">Hungry?<span>Just order foood </span><span>at your door</span></h1>
-        </Col >
+        </Col>
         <Col lg='6' md='6' className="hero-image ">
         <img src={hero}  className="w-100" alt="" />
         </Col>
 
 
 
-
-
-
-
-
-
-
       <Category/>
-
-
         </Row>
 
     </Container>
