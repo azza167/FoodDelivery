480b71fbe8f8e1298f0110353827c5eff2ca60b7
nadah79 <nada.hesham.sh@gmail.com>
2022-11-08 15:56:06 +0200

    slider

--- src/component/Slider/HomeSlider.jsx
+++ src/component/Slider/HomeSlider.jsx
@@ -3,17 +3,37 @@ import Slider from "react-slick";
 import slider1 from "../../assets/images/slider3.webp"
 import slider2 from "../../assets/images/slider2.webp"
 import slider3 from "../../assets/images/slider1.webp"
-// import "./home_slider.css"
+import { FaArrowRight, FaArrowLeft } from "react-icons/fa";
+import { Container } from 'reactstrap';
+// import {NextArrow ,PrevArrow} from "react-icons"
 
 function HomeSlider() {
+  // const NextArrow= ({onClick}) => {
+  //   return (
+  //     <div className="arrow next" onClick={onClick}>
+  //       <FaArrowRight />
+  //     </div>
+  //   )
+  // }
+
+  // const PrevArrow = ({onClick}) => {
+  //   return (
+  //     <div className="arrow prev" onClick={onClick}>
+  //       <FaArrowLeft />
+  //     </div>
+  //   )
+  // }
+  
   const settings = {
     dots: true,
     autoplay: true,
-    infinite: true,
     speed: 1000,
-    swipeToSlide: true,
     slidesToShow: 1,
     slidesToScroll: 1,
+    arrows:true,
+    //  nextArrow: <NextArrow />, 
+    //  prevArrow: <SamplePrevArrow/>,
+
   };
   return (
     <div className="">
