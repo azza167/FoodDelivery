654053db7ed442c339d223ec64e77c5c4cf0a0b2
M-Metaw3 <mizoomizoo161@gmail.com>
2022-11-11 16:53:10 +0200

    changes

--- src/component/Header/header.jsx
+++ src/component/Header/header.jsx
@@ -1,12 +1,11 @@
+import React from 'react';
+import { Container } from 'reactstrap';
+import Logo from '../../assets/images/icon1.png'
+import { useSelector,useDispatch} from 'react-redux';
 import {NavLink,Link} from 'react-router-dom';
 import '../../Styles/Header.css'
 import { useEffect, useState } from 'react'
-import { getAuth, updatePassword } from "firebase/auth";
-
 import { auth } from '../../firebase';
-
-
-
 const nav__links=[
   {
     display:'Home',
@@ -24,54 +23,35 @@ const nav__links=[
     display: "Contact",
     path: "/contact",
   },
-
 ]
-
 const Header = () => {
-
-
-
-
   var cartTotal = useSelector((state)=>state);
-
 const [a,seta]=useState(null)
 const [email,setemail]=useState('')
-
 const[show , setshow]=useState(false)
-
- 
 const dispatch = useDispatch();
 // const user=useSelector((state)=>state.userauto)
-
 useEffect(()=>{
   auth.onAuthStateChanged((userr)=>{
-    
     userr?setemail(userr.email):setemail('')
    userr.email==='metaea@gmail.com'?setshow(true):setshow(false)
-
-   
- 
       })
 },[]) 
-
 const handeler=()=>{
   const obj ={
-    
     type:"showCart",
   };
   dispatch(obj);
 }
-
   return (
     <header className="header">
   <Container>
-  
       <div className='nav-wrapper d-flex align-items-center justify-content-between' >
         <div className='logo'>
         <img src={Logo} alt='logo' />
        {   /* <h5> Tasty Treat</h5>*/}
         </div> 
-        {/Menu/}
+        {/* {/Menu/} */}
         <div className="navigation" >
         <div className="menu d-flex align-items-center gap-5">
           {nav__links.map((item, index) => (
@@ -83,51 +63,33 @@ const handeler=()=>{
               }
             >
               {item.display}
-              
             </NavLink>
           ))}
           {show?
          <NavLink
               to={'/admain'}
-
             >
             Admain
             </NavLink>
 :''}
-       
-           
         </div>
       </div>
        {/* nav right icons */}
-       
        <div className="nav__right d-flex align-items-center gap-4">
        <span className="cart__icon"
+         onClick={()=>{handeler()}}
        >
-        
        </span>
-       <span className="cart__icon"
-              onClick={()=>handeler()}
-       >
-
-        <NavLink>
+       <span className="cart__icon">
+       <NavLink to={'/Cart'}>
          <i className="ri-shopping-basket-line"></i>
-        </NavLink>
-         <span className="cart__badge">
-          
-         </span>
-
-
+    </NavLink>
+         <span className="cart__badge"></span>
          <span className="cart__badge"
-
          >
-         
          <Link to=""></Link>
          {cartTotal.AddTocart.length}</span>
-
          <span className="cart__badge">  {cartTotal.AddTocart.length}</span>
-         
-       
-
          <Link to=""></Link>
          <span className="cart__badge">  {cartTotal.AddTocart.length}</span>         
        </span>
@@ -136,19 +98,13 @@ const handeler=()=>{
            <i className="ri-user-line" ><span style={{fontSize:'10px'}}>{email}</span></i>     
          </Link>
        </span>
-
        <span className="mobile__menu">
          <i className="ri-menu-line"></i>
        </span>
      </div>
-      
       </div>
   </Container>
-  
   </header>
-    
-  
   )
 }
-
 export default Header
--- src/pages/Updatepassord.jsx
+++ src/pages/Updatepassord.jsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import { getAuth, updatePassword } from "firebase/auth";
 import  { useEffect, useState } from 'react'
-
+import { Container } from 'reactstrap';
 import { auth } from '../firebase';
 const Updatepassord = () => {
 
