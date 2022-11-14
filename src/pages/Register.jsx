d93f4879d7a92a6ebc91c0652fc074d511cde234
M-Metaw3 <mizoomizoo161@gmail.com>
2022-11-14 19:17:57 +0200

    ROUT

--- src/pages/Register.jsx
+++ src/pages/Register.jsx
@@ -8,6 +8,7 @@ import React from 'react'
 import { Container, Row, Col } from "reactstrap";
 import { Link } from "react-router-dom";
 const Register = () => {
+
   const initial ={email:'',password:'',repassword:''}
   const[inp,setinp]=useState(initial)
   const dispatch =useDispatch()
@@ -34,9 +35,6 @@ const Register = () => {
   storage.getDownloadURL().then((el)=>setdownload(el))
   })
   }
-
-
-
 
   
   const[discount,setdiscount]=useState('')
